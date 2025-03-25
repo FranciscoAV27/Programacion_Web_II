e52c2b4c08d9436ce3682eb0d30e5bdc775ed190
FranciscoAV27 <franchesco497@gmail.com>
2025-03-25 15:51:51 -0600

    Agregando mi_carpeta al repositorio

---
+++ Subir/Practicas/p1/expo1_Alo/myFun.js
@@ -0,0 +1,9 @@
+function cambiar() {
+    const imagen = document.getElementById("miImagen");
+    imagen.src = "R.jpeg";
+}
+
+function revertir() {
+    const imagen = document.getElementById("miImagen");
+    imagen.src = "Foco-LED-off.jpg";
+}
